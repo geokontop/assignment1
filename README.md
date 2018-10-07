88afb6d04629c729973db7d666dee2c599644092
unknown <gkontopulos@gmail.com>
2018-10-07 20:25:02 +0300

    1st commit

---
+++ README.md
@@ -0,0 +1,29 @@
+#Assignment_1
+
+## Steps
+1. Create an http server
+1. Server listens on port 3010
+1. Inside createServer 
+1.1. Get URL and parse it
+1.1. Extract path from URL
+1.1. Trimm '/' from begining and end
+1.1. Initialize variable buffer that will accumulate the incoming stream - decoded
+1.1. Bind to  event 'data' that the request object emmits
+1.1.1. In the event callback we collect the incoming payload stream, if any
+1.1. Bind to  event 'end' that the request object emmits
+1.1.1. it's callback is called when the incoming stream compleets. In it we will handle the response and log the activity
+1. Define a request router object
+1.1. In it define path (key) 'hello' with the appropriate hello handler as value
+1. Define a handlers object with
+1.1 hello handler function, returns statusCode 200 and a payload
+1.1 notFound handler function, returns statusCode 404
+1. Write the server response inside request end callback. 
+1.1. Choose the handler 
+1.1. Construct the response data
+1.1. Route accordingly calling the chosen handler
+1.1.1. Pass the constructed response data to be used by the handler (just in case)
+1.1.1. Use the statusCode if provided by the handler, otherways define some default
+1.1.1. Use the payload if provided, otherways define some default
+1.1.1. Convert payload to string
+1.1.1. Write response object
+1.1.1. In the response header set Content-Type, application/json
---
+++ index.js
@@ -0,0 +1,100 @@
+/*
+ * 1st assignment
+ * 
+ */
+ 
+
+ // Dependencies
+
+ var http = require('http');
+ var url = require('url');
+ var StringDecoder = require('string_decoder').StringDecoder;
+
+var server = http.createServer((req,res)=>{
+    
+    // Get the URL and parse it
+    var parsedUrl = url.parse(req.url,true);
+
+    // Get the path
+    var path = parsedUrl.pathname;
+    var trimmedPath = path.replace(/^\/+|\/+$/g, '');
+
+    
+
+    // Get the querry string as an object
+    var querryStringObject = parsedUrl.query;
+
+    // Get the http method
+    var method = req.method.toLowerCase();
+
+    // Get the headers as an object
+    var headers = req.headers;
+
+    // Get the payload, if any
+    var decoder = new StringDecoder('utf-8');
+    var buffer = '';
+    req.on('data',(data)=>{
+        buffer += decoder.write(data);
+    });
+    req.on('end',()=>{
+        buffer += decoder.end();
+        
+        // Choose the handler the request should go to. If one is not found, use the notFound handler.
+        var chosenHandler = typeof(router[trimmedPath])!= 'undefined'? router[trimmedPath]:handlers.notFound;
+
+        // Construct the data-object to send to the handler
+        var data = {
+            'trimmedPath' : trimmedPath,
+            'querryStringObject' : querryStringObject,
+            'method': method,
+            'headers': headers,
+            'payload' : buffer
+        };
+
+        // Route the request to the chosen handler  
+        chosenHandler(data, (statusCode, payload)=>{
+            // Use the status code called back by the handler or default to 200
+            statusCode= typeof(statusCode) == 'number'? statusCode:200;
+
+            // Use the payload called back by the handler or default to empty object
+            payload= typeof(payload) == 'object'? payload:{};
+
+            // Convert the payload to a string
+            payloadString = JSON.stringify(payload);
+
+            // Write response
+            res.setHeader('Content-Type', 'application/json');
+            res.writeHead(statusCode);
+            res.end(payloadString)
+
+            // Log activity
+            console.log('method:', data.method, 'request received on path:', data.trimmedPath)
+
+        })
+    })
+
+})
+
+server.listen(3010,()=>{
+    console.log('The server is listening on port 3010')
+})
+
+
+// Define the handlers
+var handlers = {};
+
+// Sample handler
+handlers.hello =(data, callback)=>{
+    // Callback a http status code, and a payload object
+    callback(200,{'name': 'hello handler'});
+}
+
+// Not found handler
+handlers.notFound = (data, callback)=>{
+    callback(404);
+}
+
+// Define a request router
+var router = {
+    'hello' : handlers.hello
+}
